a99b243d682f44d86466d42410c559a3c0d9720b
KamilRizatdinov <k.rizatdinov@innopolis.university>
2022-04-16 22:45:24 +0300

    feat: add assemblyscript simple case

--- src/cases/simple/assemblyscript/bench.js
+++ src/cases/simple/assemblyscript/bench.js
@@ -1,10 +1,22 @@
 import { benchmarkWrapper, mean, peak, min } from "../../../utils/benchmarkWrapper";
-import { add } from "./index.js";
+
+// const ascModule = new WebAssembly.Module("./index.wasm");
+const ascModule = new WebAssembly.Module(readbuffer(arguments[0]));
 
 const results = await benchmarkWrapper({
-  async run() {
-    add(1,2);
+  async before() {
+    this.instance = new WebAssembly.Instance(ascModule, {
+      env: {
+        abort() {
+          throw Error("ARRGGH");
+        },
+      },
+    });
   },
+  async run() {
+    this.instance.exports.add(1, 2);
+  },
+  numWarmup: 0,
 });
 
 console.log([mean(results), peak(results), min(results)].join());
--- src/cases/simple/assemblyscript/index.ts
+++ src/cases/simple/assemblyscript/index.ts
@@ -1,3 +1,6 @@
 export function add(a: i32, b: i32): i32 {
+  for (let i = 0; i <= 10000000; i++) {
+    continue;
+  }
   return a + b;
 }
