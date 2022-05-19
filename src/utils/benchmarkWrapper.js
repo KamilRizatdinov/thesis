c32ba19acc6bd0c842c1bb8870fc25b5f0eca63d
KamilRizatdinov <k.rizatdinov@innopolis.university>
2022-05-19 14:31:33 +0300

    refactor: simplify benchmark wrapper

--- src/utils/benchmarkWrapper.js
+++ src/utils/benchmarkWrapper.js
@@ -1,20 +1,14 @@
 const numIterationsDefault = 50;
 
 export async function benchmarkWrapper({
-  before = () => {},
   run = () => {},
-  after = () => {},
   numIterations = numIterationsDefault,
 } = {}) {
   const results = [];
-
   for (let i = 0; i < numIterations; i++) {
-    const context = {};
-    await before.call(context);
     const start = Date.now();
-    await run.call(context);
+    await run();
     results.push(Date.now() - start);
-    await after.call(context);
   }
   return results;
 }
